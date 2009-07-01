b243f59e739e107ef2a3a2ccd3ac18ab374f8e3d
karlcpp <karlcpp@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-01 16:16:01 +0000

    1. 接收红蓝双方发来的消息，并插入到各自的指令队列中。 2. 修改了消息格式，每个发送的消息元组的第一个参数是进程的ID， 这样就不会有人冒充别人发消息了。

--- trunk/eb0/erlbattle.erl
+++ trunk/eb0/erlbattle.erl
@@ -1,6 +1,22 @@
 -module(erlbattle).
 -export([start/0,timer/3]).
 
+%% 定义了一个打印调试信息的宏
+-define(debug_print(Level, Str),
+    fun() ->
+        case Level of
+            fatal   -> io:format("FATAL\t ~p:~p ~n\t~p~n", [?FILE, ?LINE, Str]);
+            error   -> io:format("ERROR\t ~p:~p ~n\t~p~n", [?FILE, ?LINE, Str]);
+            notice  -> io:format("NOTICE\t ~p:~p ~n\t~p~n",[?FILE, ?LINE, Str]);
+            info    -> io:format("INFO\t ~p:~p ~n\t~p~n", [?FILE, ?LINE, Str]);
+            true -> ok
+        end
+    end()).
+    
+%% 命令记录
+%% TODO: 建议把这些记录和宏定义放到一个统一的.hrl文件中，各模块一起使用
+-record(command, {warrior_id, command_name, execute_time}).
+
 %% 战场初始化启动程序
 start() ->
     io:format("Server Starting ....~n", []),
@@ -13,11 +29,11 @@ start() ->
 	Sleep = 10,
 
 	%% 启动一个计时器, 作为战场节拍
-	spawn(erlbattle, timer, [self(),1,Sleep]),
+	Timer = spawn(erlbattle, timer, [self(),1,Sleep]),
 
 	%% 创建两个指令队列， 这两个队列只能由各自看到
-	BlueQueue = ets:new(blueQueue, []),
-	RedQueue = ets:new(redQueue, []),
+	BlueQueue = ets:new(blueQueue, [{keypos, #command.warrior_id}]),
+	RedQueue = ets:new(redQueue, [{keypos, #command.warrior_id}]),
 	
 	%% 启动红方和蓝方的决策程序
 	%% TODO:  为了避免某一方通过狂发消息，影响对方， 未来要有独立的通讯程序负责每方的信息
@@ -27,31 +43,53 @@ start() ->
 	
 
 	%% 开始战场循环
-	run(BlueSide, RedSide,BlueQueue, RedQueue).
+	run(Timer, BlueSide, RedSide,BlueQueue, RedQueue).
 		
 
-%% 战场逻辑主程序	
-run(BlueSide, RedSide,BlueQueue, RedQueue) ->
-	
+%% 战场逻辑主程序
+run(Timer, BlueSide, RedSide, BlueQueue, RedQueue) ->
 	receive 
-		finish ->
+		{Timer, finish} ->
 			BlueSide!finish,
 			RedSide!finish,
 			io:format("Sun goes down, battle finished!~n", []),
 			%% 输出战斗结果
 			io:format("The winner is blue army ....~n", []);			
-		{time, Time} ->
+		{Timer, time, Time} ->
 				%% TODO 战场逻辑
 				%% do something
 				io:format("Time: ~p s ....~n", [Time]),
+                                
 				%% For Test, 从ETS表中读取并显示战场时钟
-				io:format("Battle Clock: ~p ...~n", [ets:lookup(battle_timer, clock)]),
-				run(BlueSide, RedSide,BlueQueue, RedQueue);
-		{command,Command,Warrior,Time} ->
-				%% Todo 接受消息
-				io:format("~p warrior want ~p at ~p ~n", [Warrior, Command, Time]),
-				
-				run(BlueSide, RedSide,BlueQueue, RedQueue)
+                                %% 这里好像有一个问题，当我把timer的最大值调到25时，
+                                %% 在这里打印战场时钟时，有时程序为崩溃
+                                %% 我在windows下测试的。
+                                %%?debug_print(info, ets:lookup(battle_timer, clock)),
+				run(Timer, BlueSide, RedSide,BlueQueue, RedQueue);
+		{Side, command,Command,Warrior,Time} ->
+				%% 生成一个command 记录
+                                CmdRec = #command{
+                                        warrior_id = Warrior,
+                                        command_name = Command,
+                                        execute_time = Time},
+                                case Side of
+                                    %% 蓝方发来的命令
+                                    BlueSide ->
+                                        io:format("BlueSide: ~p warrior want ~p at ~p ~n", [Warrior, Command, Time]),
+                                        ets:insert(BlueQueue, CmdRec),
+                                        ?debug_print(info, ets:tab2list(BlueQueue)),
+                                        run(Timer, BlueSide, RedSide,BlueQueue, RedQueue);
+                                    %% 红方发来的命令
+                                    RedSide ->
+                                        io:format("RedSide: ~p warrior want ~p at ~p ~n", [Warrior, Command, Time]),
+                                        ets:insert(RedQueue, CmdRec),
+                                        ?debug_print(info, ets:tab2list(RedQueue)),
+                                        run(Timer, BlueSide, RedSide,BlueQueue, RedQueue);
+                                    %% 不知道是那一方发来的命令
+                                    _ ->
+                                        io:format("UnknowSide: ~p warrior want ~p at ~p ~n", [Warrior, Command, Time]),
+                                        run(Timer, BlueSide, RedSide,BlueQueue, RedQueue)
+                                end
 	end.
 
 %% Todo: Sleep 小程序,休息若干毫秒
@@ -59,7 +97,7 @@ timer(Pid, Time,Sleep) ->
 	sleep(Sleep),
 	
 	%% 战场最多运行的次数 
-	MaxTurn = 5,
+	MaxTurn = 25,
 	%% 第一次启动，初始化battle_timer表
 	if 
 		Time == 1 ->
@@ -72,9 +110,9 @@ timer(Pid, Time,Sleep) ->
 
 	if    
 		Time == MaxTurn ->
-			Pid!finish;
+			Pid!{self(), finish};
 		Time < MaxTurn ->
-			Pid !{time, Time},
+			Pid !{self(), time, Time},
 			timer(Pid, Time+1,Sleep)
 	end.
 
