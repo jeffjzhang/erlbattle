aabdb877228948341c6d4d528534b85e4fd2ad1a
fanyun2000 <fanyun2000@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-03 13:22:42 +0000

    修正timer 问题

--- eb0/timer.erl
+++ eb0/timer.erl
@@ -1,14 +1,15 @@
 -module(timer).
--export([timer/3,getTime/0]).
+-include("schema.hrl").
+-export([start/3,getTime/0]).
 
 %% Todo: Sleep 小程序,休息若干毫秒
-timer(Pid, 0, Sleep) ->
+start(Pid,0,Sleep) ->
 	%% 第一次启动，初始化battle_timer表
 	ets:new(battle_timer, [set, protected, named_table]),
 	ets:insert(battle_timer, {clock, 0}),
-	timer(Pid, Sleep).
+	loop(Pid, Sleep).
 
-timer(Pid, Sleep) -> 
+loop(Pid, Sleep) -> 
 	tools:sleep(Sleep),
 	
 	%% 战场最多运行的次数 
@@ -16,13 +17,13 @@ timer(Pid, Sleep) ->
 
 	%% 更新clock值
 	Time = ets:update_counter(battle_timer, clock, 1),
-
+	
 	if    
 		Time == MaxTurn ->
 			Pid!{self(), finish};
 		Time < MaxTurn ->
 			Pid !{self(), time, Time},
-			timer(Pid, Sleep)
+			loop(Pid, Sleep)
 	end.
 
 %% 取时间函数
