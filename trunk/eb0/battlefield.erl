03bfcd89b0538eafb0c892767147d30101df2561
fanyun2000 <fanyun2000@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-03 09:25:41 +0000

    将battle field 创建的测试程序剥离出来

--- trunk/eb0/battlefield.erl
+++ trunk/eb0/battlefield.erl
@@ -1,7 +1,6 @@
 -module(battlefield).
 -author("swingbach@gmail.com").
--export([create/0,get_soldier/2,get_soldier_inbattle/1,test/0]).
--include("test.hrl").
+-export([create/0,get_soldier/2,get_soldier_inbattle/1]).
 -include("schema.hrl").
 
 create() ->
@@ -52,36 +51,6 @@ get_soldier_inbattle(Position) ->
 			none
 	end.
 
-%%测试根据ID查找战士属性
-test10()->
-	%%战队错误
-	?match(none,get_soldier(1,"sdf")),
-	%%成功取到信息
-	Soldier=#soldier{
-				id={2,"red"},
-				position={0,2+2},
-				hp=100,
-				direction="E",
-				action="wait"
-			},
-	?match(Soldier,get_soldier(2,"red")).
 
-%%测试根据坐标位置查找战士属性
-test20()->
-	?match(none,get_soldier_inbattle({1,14})),
-	?match(none,get_soldier_inbattle({14,0})),
-	Soldier=#soldier{
-			id={7,"blue"},
-			position={14,9},
-			hp=100,
-			direction="W",
-			action="wait"
-		},
-	?match(Soldier,get_soldier_inbattle({14,9})).
-
-test()->
-	create(),
-	test10(),
-	test20().
 
 
---
+++ trunk/eb0/testBattleFieldCreate.erl
@@ -0,0 +1,38 @@
+-module(testBattleFieldCreate).
+-export([test/0]).
+-include("test.hrl").
+-include("schema.hrl").
+
+%% 测试
+test()->
+	battlefield:create(),
+	test10(),
+	test20().
+	
+%%测试根据ID查找战士属性
+test10()->
+	%%战队错误
+	?match(none,battlefield:get_soldier(1,"sdf")),
+	%%成功取到信息
+	Soldier=#soldier{
+				id={2,"red"},
+				position={0,2+2},
+				hp=100,
+				direction="E",
+				action="wait"
+			},
+	?match(Soldier,battlefield:get_soldier(2,"red")).
+
+%%测试根据坐标位置查找战士属性
+test20()->
+	?match(none,battlefield:get_soldier_inbattle({1,14})),
+	?match(none,battlefield:get_soldier_inbattle({14,0})),
+	Soldier=#soldier{
+			id={7,"blue"},
+			position={14,9},
+			hp=100,
+			direction="W",
+			action="wait"
+		},
+	?match(Soldier,battlefield:get_soldier_inbattle({14,9})).
+
