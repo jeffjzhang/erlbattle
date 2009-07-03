d31425f1ca3578f704f93818f3690d9c4a5a7099
fanyun2000 <fanyun2000@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-03 14:29:27 +0000

    修改winmake 让他能够编译测试代码。同时运行所有测试。

--- eb0/WinMake.bat
+++ eb0/WinMake.bat
@@ -1,1 +1,3 @@
-erlc -W erlbattle.erl battlefield.erl tools.erl worldclock.erl englandArmy.erl feardFarmers.erl testWorldClockGetTime.erl testBattleFieldCreate.erl
+erlc -W erlbattle.erl battlefield.erl tools.erl worldclock.erl englandArmy.erl feardFarmers.erl 
+erlc -W testAll.erl testWorldClockGetTime.erl testBattleFieldCreate.erl
+erl -noshell -s testAll test -s init stop
