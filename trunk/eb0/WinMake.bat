8c68283fbf0edb07184b388118a1f8ff7eb3d551
fanyun2000 <fanyun2000@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-06 09:50:27 +0000

    修正原有自动测试中的错误。 修改winmake 文件，让其能够自动测试takeAction 代码

--- trunk/eb0/WinMake.bat
+++ trunk/eb0/WinMake.bat
@@ -1,3 +1,4 @@
+del *.beam *.dump
 erlc -W erlbattle.erl battlefield.erl tools.erl worldclock.erl englandArmy.erl feardFarmers.erl 
-erlc -W testAll.erl testWorldClockGetTime.erl testBattleFieldCreate.erl
+erlc -W testAll.erl testWorldClockGetTime.erl testBattleFieldCreate.erl testErlBattleTakeAction.erl
 erl -noshell -s testAll test -s init stop
