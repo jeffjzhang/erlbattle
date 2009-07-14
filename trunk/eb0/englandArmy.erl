3b33243c61a8f659e7d17fe057106509885ae69c
fanyun2000 <fanyun2000@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-14 13:25:54 +0000

    最终完成englandArmy ， 并测试一场完整的战争

--- trunk/eb0/englandArmy.erl
+++ trunk/eb0/englandArmy.erl
@@ -1,4 +1,5 @@
 -module(englandArmy).
+-include("schema.hrl").
 -export([run/2]).
 
 run(Channel, Side) ->
@@ -10,22 +11,27 @@ run(Channel, Side) ->
 
 loop(Channel, Side) ->
 	
-	Channel!{command,"forward",1,0},
-	Channel!{command,"forward",2,0},
-	Channel!{command,"forward",3,0},
-	Channel!{command,"forward",4,0},
-	Channel!{command,"forward",5,0},
-	Channel!{command,"forward",6,0},
-	Channel!{command,"forward",7,0},
-	Channel!{command,"forward",8,0},
-	Channel!{command,"forward",9,0},
-	Channel!{command,"forward",10,0},
+	Army = [1,2,3,4,5,6,7,8,9,10],
 	
+	lists:foreach(
+		fun(Soldier) ->   % 一直朝前， 直到碰到人，然后开始砍	
+			case someoneAhead(Soldier,Side) of
+				true ->
+					Channel!{command,"attack",Soldier,0};
+				false ->
+					Channel!{command,"forward",Soldier,0};
+				_ ->
+					none
+			end
+		end,
+		Army),
+	
+	%% 等待结束指令，其实这个程序不需要做任何善后，只是作为例子提供给大家模仿
 	receive
 		%% 结束战斗，可以做一些收尾工作后退出，或者什么都不做
 		%% 这个消息不是必须处理的
 		{'EXIT',_FROM, finish} ->  
-			io:format("england Army draw back ~n",[]);
+			io:format("England Army Go Back To Castle ~n",[]);
 					
 		_ ->
 			loop(Channel, Side)
@@ -34,4 +40,34 @@ loop(Channel, Side) ->
 			loop(Channel, Side)
 			
 	end.
+
+
+%% 计算某个角色前面是否有人
+someoneAhead(SoldierId,Side) ->
+	
+	case battlefield:get_soldier(SoldierId,Side) of
+		
+		none ->  % 角色不存在（已经挂掉了）
+			none;
+		
+		Soldier when is_record(Soldier,soldier) ->  % 找到角色
+
+			Position = erlbattle:calcDestination(Soldier#soldier.position, Soldier#soldier.facing, 1),
+
+			case battlefield:get_soldier_by_position(Position) of 
+				none ->  		%前面没人
+					false;
+				_Found ->		%有人
+					true
+			end;
+		_->
+			none
+	end.
+					
+			
+			
+	
+
+
+
 	
