f37e99029fe40d9ee0a3380152f40b1030b4494b
fanyun2000 <fanyun2000@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-18 18:06:02 +0000

    基本完成所有主程序开发。

---
+++ branches/eb0.0719/eb0/battlefield.erl
@@ -0,0 +1,85 @@
+-module(battlefield).
+-author("swingbach@gmail.com").
+-export([create/0,get_soldier/2,get_soldier_by_position/1,get_soldier_by_side/1,get_idle_soldier/1]).
+-include("schema.hrl").
+
+create() ->
+	%%创建战场信息表，用于查找战士信息，及某坐标点信息
+	ets:new(battle_field,[named_table,protected,{keypos,#soldier.id}]),
+    %%初始化士兵及位置
+	init_soldier("red",0,1,"east"),
+	init_soldier("blue",14,1,"west").
+
+init_soldier(Army,X,Y,Direction)->
+	Soldiers=[1,2,3,4,5,6,7,8,9,10],
+	lists:foreach(
+		fun(Id) ->
+			Soldier=#soldier{
+				id={Id,Army},
+				position={X,Y+Id},
+				hp=100,
+				facing = Direction,
+				action="wait",
+				act_effect_time = 0,
+				act_sequence =0
+			},
+			ets:insert(battle_field,Soldier)
+		end,
+		Soldiers).
+
+%%根据战士编号及战队得到该战士信息
+get_soldier(Id,Side) ->
+	case ets:lookup(battle_field,{Id,Side}) of
+		[Soldier] ->
+			Soldier;
+		[]->
+			none
+	end.
+
+%%得到某个坐标点上战士全部信息
+get_soldier_by_position(Position) ->
+	Pattern=#soldier{
+				id='_',
+				position=Position,
+				hp='_',
+				facing='_',
+				action='_',
+				act_effect_time = '_',
+				act_sequence = '_'
+			},
+	
+	case ets:match_object(battle_field,Pattern) of
+		[Soldier | _Other] ->
+			Soldier;
+		[]->
+			none
+	end.
+
+%%获得某方所有战士列表
+get_soldier_by_side(Side) ->
+	Pattern=#soldier{
+				id={'_',Side},
+				position='_',
+				hp='_',
+				facing='_',
+				action='_',
+				act_effect_time = '_',
+				act_sequence = '_'
+			},
+	
+	ets:match_object(battle_field,Pattern).
+
+%%获得某方所有处于wait 状态的战士列表
+get_idle_soldier(Side) ->
+	Pattern=#soldier{
+				id={'_',Side},
+				position='_',
+				hp='_',
+				facing='_',
+				action="wait",
+				act_effect_time = '_',
+				act_sequence = '_'
+			},
+	
+	ets:match_object(battle_field,Pattern).
+
