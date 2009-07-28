b0a330d732d39bd986b4a43901ff81827fefee43
fanyun2000@gmail.com <fanyun2000@gmail.com@3c52b02c-5faf-11de-bc6d-35d2ebd5fd4e>
2009-07-28 14:58:02 +0000

    去掉madDog 中 lists:keyfind 函数。 改为 tools:keyfind

--- trunk/eb0/tools.erl
+++ trunk/eb0/tools.erl
@@ -1,5 +1,5 @@
 -module(tools).
--export([sleep/1,getLongDate/0]).
+-export([sleep/1,getLongDate/0,keyfind/3]).
 
 %% Sleep 工具函数
 sleep(Sleep) ->
@@ -14,3 +14,12 @@ getLongDate() ->
 	Seed = 1000 * 1000,
 	(MegaSecs * Seed + Secs) *Seed + MicroSecs.
 	
+%% 兼容代码
+keyfind(Key, N, List) ->
+	
+	case lists:keysearch(Key, N, List) of
+		
+		false -> false;
+		{_Val, Result} -> Result;
+		_ELSE -> false
+	end.
